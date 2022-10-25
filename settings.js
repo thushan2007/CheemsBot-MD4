bce865eb63cd5b57603c12f3c57cb61c205c5f05
thushan2007 <116637811+thushan2007@users.noreply.github.com>
2022-10-25 19:32:10 +0530

    New futuers
    New names

--- settings.js
+++ settings.js
@@ -61,16 +61,16 @@ global.socialm = "GitHub: DGXeon" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //bot bomdy 
-global.owner = ['916909137213']
-global.ownertag = '916909137213' //ur tag number
-global.botname = 'Cheems Bot MD V4' //ur bot name
-global.linkz = "https://youtu.be/WiIqCdiDjFo" //your theme url which will be displayed on whatsapp
+global.owner = ['0765526449']
+global.ownertag = '0765526449' //ur tag number
+global.botname = 'TD SHIBA BOT' //ur bot name
+global.linkz = "https://youtube.com/channel/UCMWMLJ6TpD5JiY9Sl0ur1Rw" //your theme url which will be displayed on whatsapp
 global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
 global.botscript = 'https://github.com/DGXeon/CheemsBot-MD4' //script link
-global.reactmoji = "😘" //ur menu react emoji
-global.themeemoji = "🦄" //ur theme emoji
-global.packname = "Cheems Bot Stickers\n\n\n\n\n\n\n\n\n\n\n🦄🦄🦄 " //ur sticker watermark packname
-global.author = "🦄Dream Guy Xeon" //ur sticker watermark author
+global.reactmoji = "☑️" //ur menu react emoji
+global.themeemoji = "🐶" //ur theme emoji
+global.packname = "Cheems Bot Stickers\n\n\n\n\n\n\n\n\n\n\n🐶" //ur sticker watermark packname
+global.author = "🐶Dream Guy Xeon" //ur sticker watermark author
 
 //Bot theme media
 global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
